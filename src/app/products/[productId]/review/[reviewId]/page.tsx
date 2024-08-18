b78cbaeb9233019bf354601ba6e8c905d575662a
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-08-18 21:03:43 +0700

    error tsx

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,6 +1,10 @@
 import { notFound } from "next/navigation";
 
-export default function ReviewId({
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
+export default function ReviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
@@ -8,9 +12,14 @@ export default function ReviewId({
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   return (
-    <h1 className="text-4xl">
-      Review {params.reviewId} & product {params.productId}
+    <h1 className="text-4xl font-bold">
+      Review {params.reviewId} for product {params.productId}
     </h1>
   );
 }
