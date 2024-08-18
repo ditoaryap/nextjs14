53ae931a4f9d9c523595efab5b86295bc7f1d00d
Dito Aryaputra Ramadhani <ditoaryaputra1324@gmail.com>
2024-08-18 20:41:31 +0700

    useRouter

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -18,8 +17,13 @@ export default function AuthLayout({
   return (
     <div>
       {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
         return (
-          <Link href={link.href} key={link.name}>
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
             {link.name}
           </Link>
         );
